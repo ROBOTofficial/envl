edead49ef431e8c401f45e3a2b85829e63b5ecc3
ROBOT <tkata12@outlook.jp>
2025-08-06 00:29:43 +0900

    add: eslint and prettier in envl-js

--- scripts/eslint.config.js
+++ scripts/eslint.config.js
@@ -6,19 +6,11 @@ import { includeIgnoreFile } from "@eslint/compat";
 import { defineConfig } from "eslint/config";
 import { fileURLToPath } from "node:url";
 
-const gitignorePath = fileURLToPath(new URL("./.gitignore", import.meta.url));
+const gitignorePath = fileURLToPath(new URL("../.gitignore", import.meta.url));
 
-export default defineConfig([
+export const config = defineConfig([
 	includeIgnoreFile(gitignorePath),
 	prettier,
-	{
-		files: ["**/*.{js,mjs,cjs,ts}"],
-		plugins: { js },
-		extends: ["js/recommended"]
-	},
-	{
-		files: ["**/*.{js,mjs,cjs,ts}"],
-		languageOptions: { globals: globals.browser }
-	},
-	tseslint.configs.recommended
+    js.configs.recommended,
+	...tseslint.configs.recommended
 ]);
