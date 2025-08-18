afa5b70f8753c49168645e96dcd04cdbefedc48b
ROBOT <tkata12@outlook.jp>
2025-08-19 02:04:11 +0900

    fix: iter

--- packages/envl-vars/src/parser/parser.rs
+++ packages/envl-vars/src/parser/parser.rs
@@ -33,6 +33,7 @@ impl Parser {
     }
 
     pub fn parse(&self) -> Result<Vec<Variable>, ParserError> {
+        let mut tokens = self.tokens.iter();
         let mut vars = Vec::new();
         let mut equal_used = false;
         let mut comma_used = false;
@@ -68,158 +69,163 @@ impl Parser {
             };
         }
 
-        'parse_loop: for token in self.tokens.iter() {
-            let value = &token.value;
-            let position = token.position.clone();
-            match value {
-                Value::LeftBracket => {
-                    if var.name.is_some() && var.value.is_none() && equal_used && !in_array {
-                        in_array = true;
-                    } else {
-                        parser_error = Some(ParserError {
-                            code: ErrorCode::SyntaxError,
-                            message: format!("Write arrays after the equal written"),
-                            position: position.clone(),
-                        });
-                        break 'parse_loop;
-                    }
-                }
-                Value::RightBracket => {
-                    if !in_array {
-                        parser_error = Some(ParserError {
-                            code: ErrorCode::SyntaxError,
-                            message: format!("Use ] only when closing an array"),
-                            position: position.clone(),
-                        });
-                        break 'parse_loop;
-                    }
-
-                    match (var.name.clone(), var.value.clone()) {
-                        (Some(_), None) if equal_used => {
-                            var.value = Some(VariableValue::Array(current_array.clone()));
-                            current_array.clear();
-                            comma_used = false;
-                            in_array = false;
-                        }
-                        _ => {
-                            error!(position);
-                            break 'parse_loop;
-                        }
-                    }
-                }
-                Value::Comma => {
-                    if !(in_array && !comma_used && current_array.len() != 0) {
-                        parser_error = Some(ParserError {
-                            code: ErrorCode::SyntaxError,
-                            message: format!("Comma position is invalid"),
-                            position: position.clone(),
-                        });
-                        break 'parse_loop;
-                    }
-                    comma_used = true;
-                }
-                Value::Equal => {
-                    if equal_used {
-                        error!(position);
-                        break 'parse_loop;
-                    }
-                    match (&var.name, &var.value) {
-                        (Some(_), None) => {
-                            equal_used = true;
-                        }
-                        _ => {
-                            error!(position);
-                            break 'parse_loop;
-                        }
-                    }
-                }
-                Value::Semi => {
-                    if !equal_used {
-                        error!(position);
-                        break 'parse_loop;
-                    }
-                    match (&var.name, &var.value) {
-                        (Some(name), Some(value)) => {
-                            vars.push(Variable {
-                                name: name.clone(),
-                                value: value.clone(),
+        'parse_loop: loop {
+            if let Some(token) = tokens.next() {
+                let value = &token.value;
+                let position = token.position.clone();
+                match value {
+                    Value::LeftBracket => {
+                        if var.name.is_some() && var.value.is_none() && equal_used && !in_array {
+                            in_array = true;
+                        } else {
+                            parser_error = Some(ParserError {
+                                code: ErrorCode::SyntaxError,
+                                message: format!("Write arrays after the equal written"),
                                 position: position.clone(),
                             });
-                            clear!();
-                        }
-                        _ => {
-                            error!(position);
                             break 'parse_loop;
                         }
                     }
-                }
-                Value::Ident(value) => {
-                    if var.name.is_some() && var.value.is_some() {
-                        error!(position);
-                        break 'parse_loop;
+                    Value::RightBracket => {
+                        if !in_array {
+                            parser_error = Some(ParserError {
+                                code: ErrorCode::SyntaxError,
+                                message: format!("Use ] only when closing an array"),
+                                position: position.clone(),
+                            });
+                            break 'parse_loop;
+                        }
+
+                        match (var.name.clone(), var.value.clone()) {
+                            (Some(_), None) if equal_used => {
+                                var.value = Some(VariableValue::Array(current_array.clone()));
+                                current_array.clear();
+                                comma_used = false;
+                                in_array = false;
+                            }
+                            _ => {
+                                error!(position);
+                                break 'parse_loop;
+                            }
+                        }
                     }
-                    if var.name.is_none() && !equal_used {
-                        var = Var {
-                            name: Some(value.clone()),
-                            value: None,
-                        };
-                    } else if var.value.is_none() && equal_used {
-                        let var_value: VariableValue;
-                        if value.starts_with('"') && value.ends_with('"') {
-                            let mut str_value = value.clone();
-                            str_value.remove(value.len() - 1);
-                            str_value.remove(0);
-                            var_value = VariableValue::String(str_value);
-                        } else if value.starts_with('\'') && value.ends_with('\'') {
-                            let mut str_value = value.clone();
-                            str_value.remove(value.len() - 1);
-                            str_value.remove(0);
-                            if let Ok(c) = str_value.parse::<char>() {
-                                var_value = VariableValue::Char(c);
+                    Value::Comma => {
+                        if !(in_array && !comma_used && current_array.len() != 0) {
+                            parser_error = Some(ParserError {
+                                code: ErrorCode::SyntaxError,
+                                message: format!("Comma position is invalid"),
+                                position: position.clone(),
+                            });
+                            break 'parse_loop;
+                        }
+                        comma_used = true;
+                    }
+                    Value::Equal => {
+                        if equal_used {
+                            error!(position);
+                            break 'parse_loop;
+                        }
+                        match (&var.name, &var.value) {
+                            (Some(_), None) => {
+                                equal_used = true;
+                            }
+                            _ => {
+                                error!(position);
+                                break 'parse_loop;
+                            }
+                        }
+                    }
+                    Value::Semi => {
+                        if !equal_used {
+                            error!(position);
+                            break 'parse_loop;
+                        }
+                        match (&var.name, &var.value) {
+                            (Some(name), Some(value)) => {
+                                vars.push(Variable {
+                                    name: name.clone(),
+                                    value: value.clone(),
+                                    position: position.clone(),
+                                });
+                                clear!();
+                            }
+                            _ => {
+                                error!(position);
+                                break 'parse_loop;
+                            }
+                        }
+                    }
+                    Value::Ident(value) => {
+                        if var.name.is_some() && var.value.is_some() {
+                            error!(position);
+                            break 'parse_loop;
+                        }
+                        if var.name.is_none() && !equal_used {
+                            var = Var {
+                                name: Some(value.clone()),
+                                value: None,
+                            };
+                        } else if var.value.is_none() && equal_used {
+                            let var_value: VariableValue;
+                            if value.starts_with('"') && value.ends_with('"') {
+                                let mut str_value = value.clone();
+                                str_value.remove(value.len() - 1);
+                                str_value.remove(0);
+                                var_value = VariableValue::String(str_value);
+                            } else if value.starts_with('\'') && value.ends_with('\'') {
+                                let mut str_value = value.clone();
+                                str_value.remove(value.len() - 1);
+                                str_value.remove(0);
+                                if let Ok(c) = str_value.parse::<char>() {
+                                    var_value = VariableValue::Char(c);
+                                } else {
+                                    parser_error = Some(ParserError {
+                                        code: ErrorCode::MultipleCharacters,
+                                        message: "Can't input multiple characters in char"
+                                            .to_string(),
+                                        position,
+                                    });
+                                    break 'parse_loop;
+                                }
+                            } else if is_num(value.clone()) {
+                                var_value = VariableValue::Number(value.clone());
+                            } else if let Ok(b) = value.parse::<bool>() {
+                                var_value = VariableValue::Bool(b);
                             } else {
                                 parser_error = Some(ParserError {
-                                    code: ErrorCode::MultipleCharacters,
-                                    message: "Can't input multiple characters in char".to_string(),
+                                    code: ErrorCode::InvalidType,
+                                    message: "Invalid type".to_string(),
                                     position,
                                 });
                                 break 'parse_loop;
                             }
-                        } else if is_num(value.clone()) {
-                            var_value = VariableValue::Number(value.clone());
-                        } else if let Ok(b) = value.parse::<bool>() {
-                            var_value = VariableValue::Bool(b);
+
+                            if in_array {
+                                if current_array.len() != 0 && !comma_used {
+                                    parser_error = Some(ParserError {
+                                        code: ErrorCode::SyntaxError,
+                                        message: format!("Comma is required"),
+                                        position: position.clone(),
+                                    });
+                                    break 'parse_loop;
+                                }
+                                comma_used = false;
+                                current_array.push(var_value);
+                            } else {
+                                var = Var {
+                                    name: var.name,
+                                    value: Some(var_value),
+                                };
+                            }
                         } else {
-                            parser_error = Some(ParserError {
-                                code: ErrorCode::InvalidType,
-                                message: "Invalid type".to_string(),
-                                position,
-                            });
+                            error!(position);
                             break 'parse_loop;
                         }
-
-                        if in_array {
-                            if current_array.len() != 0 && !comma_used {
-                                parser_error = Some(ParserError {
-                                    code: ErrorCode::SyntaxError,
-                                    message: format!("Comma is required"),
-                                    position: position.clone(),
-                                });
-                                break 'parse_loop;
-                            }
-                            comma_used = false;
-                            current_array.push(var_value);
-                        } else {
-                            var = Var {
-                                name: var.name,
-                                value: Some(var_value),
-                            };
-                        }
-                    } else {
-                        error!(position);
-                        break 'parse_loop;
                     }
+                    _ => {}
                 }
-                _ => {}
+            } else {
+                break 'parse_loop;
             }
         }
 
