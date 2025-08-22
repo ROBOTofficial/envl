8d4fb8c55337c41cc75c2f4f184d89685bd56b91
ROBOT <tkata12@outlook.jp>
2025-08-22 15:44:45 +0900

    add: generate_ast func

--- packages/envl-vars/src/lib.rs
+++ packages/envl-vars/src/lib.rs
@@ -1,3 +1,16 @@
+use crate::{
+    lexer::lexer::Lexer,
+    misc::variable::Variable,
+    parser::parser::{Parser, ParserError},
+};
+
 pub mod lexer;
 pub mod misc;
 pub mod parser;
+
+pub fn generate_ast(file_path: String, code: String) -> Result<Vec<Variable>, ParserError> {
+    let lexer = Lexer::new(file_path, code);
+    let tokens = lexer.generate();
+    let parser = Parser::new(tokens);
+    parser.parse()
+}
