65769e8dbd4e85da874f166669f10164a184833e
ROBOT <tkata12@outlook.jp>
2025-09-05 18:26:31 +0900

    fix: add .gitignore for envl.rs file

--- tests/envl-rs-test/src/envl.rs
+++
@@ -1,2 +0,0 @@
-# [deny (clippy :: all)] # [derive (Debug , Clone)] # [rustfmt :: skip] pub struct Env { pub a : String , pub c : bool , pub b : i64 , pub d : Vec < i64 > , } # [rustfmt :: skip] pub fn envl () -> Env { Env { a : String :: from ("123") , b : 123 , c : true , d : Vec :: from ([123 , 456 ,]) , } }
-
