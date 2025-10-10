54cf3f10df27460e23fabf50456493077a0f166e
ROBOT <tkata12@outlook.jp>
2025-10-11 00:34:16 +0900

    add: is_valid_variable_name

--- packages/envl-utils/src/lib.rs
+++ packages/envl-utils/src/lib.rs
@@ -1,3 +1,4 @@
 pub mod case;
 pub mod error;
+pub mod name;
 pub mod types;
---
+++ packages/envl-utils/src/name.rs
@@ -0,0 +1,26 @@
+use regex::RegexBuilder;
+
+pub fn is_valid_variable_name(name: &str) -> bool {
+    if let Ok(regex) = RegexBuilder::new(r"^[A-Za-z_]+$").build() {
+        regex.is_match(name)
+    } else {
+        false
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::name::is_valid_variable_name;
+
+    #[test]
+    fn variable_name_check() {
+        assert!(is_valid_variable_name("abcdef"));
+        assert!(is_valid_variable_name("abc_def"));
+        assert!(is_valid_variable_name("AbCdEf"));
+        assert!(is_valid_variable_name("AbC_dEf"));
+
+        assert!(!is_valid_variable_name(""));
+        assert!(!is_valid_variable_name("abc?def"));
+        assert!(!is_valid_variable_name("abc[]"));
+    }
+}
