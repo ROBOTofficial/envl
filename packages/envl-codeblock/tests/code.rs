0684ada023a93a95f2e639e70568355c298ef1c1
ROBOT <tkata12@outlook.jp>
2025-10-24 01:46:49 +0900

    add: enhance codeblock tests for additional language support

--- packages/envl-codeblock/tests/code.rs
+++ packages/envl-codeblock/tests/code.rs
@@ -7,6 +7,21 @@ mod test {
         let code = code_block! {
             pub fn a() {}
         };
+
         assert_eq!(code.to_string(), "pub fn a () { }");
+    }
+
+    #[test]
+    fn other_lang_codeblock_test() {
+        let code = code_block! {
+            export function a() {
+                console.log("Hello World!!");
+            }
+        };
+
+        assert_eq!(
+            code.to_string(),
+            "export function a () { console . log (\"Hello World!!\") ; }"
+        );
     }
 }
