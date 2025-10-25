bca0ea5cdafa4b1293b4457705fd3ae13b906555
ROBOT <tkata12@outlook.jp>
2025-10-25 18:11:31 +0900

    refactor: replace to_snake_case function with SnakeCase struct implementation

--- packages/envl-utils/src/case.rs
+++ packages/envl-utils/src/case.rs
@@ -1,51 +1,87 @@
-pub fn to_snake_case(txt: &str) -> String {
-    let mut result = String::new();
-
-    for (i, c) in txt.char_indices() {
-        let converted = if c.is_uppercase() {
-            if i == 0 {
-                c.to_lowercase().to_string()
-            } else {
-                format!("_{}", c.to_lowercase())
-            }
-        } else {
-            c.to_string()
-        };
-        result.push_str(&converted);
-    }
-
-    result
+pub trait Case {
+    fn gen(splited_txt: Vec<String>) -> String;
+    fn parse(txt: &str) -> Vec<String>;
 }
 
-pub fn to_camel_case(txt: &str) -> String {
-    let mut result = String::new();
-    let mut is_target = false;
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct SnakeCase;
 
-    for c in txt.chars() {
-        if c == '_' {
-            is_target = true;
-        } else if is_target {
-            result.push_str(&c.to_uppercase().to_string());
-            is_target = false;
-        } else {
-            result.push(c);
+impl Case for SnakeCase {
+    fn gen(splited_txt: Vec<String>) -> String {
+        let mut result = String::new();
+
+        for (i, txt) in splited_txt.iter().enumerate() {
+            if i != 0 {
+                result.push('_');
+            }
+            for c in txt.chars() {
+                result.push_str(&c.to_lowercase().to_string());
+            }
         }
+
+        result
     }
 
-    result
+    fn parse(txt: &str) -> Vec<String> {
+        txt.split("_").map(|e| e.to_string()).collect::<Vec<_>>()
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct CamelCase;
+
+impl Case for CamelCase {
+    fn gen(splited_txt: Vec<String>) -> String {
+        let mut result = String::new();
+
+        for (i, txt) in splited_txt.iter().enumerate() {
+            for (txt_i, c) in txt.char_indices() {
+                let c = if i != 0 && txt_i == 0 {
+                    c.to_uppercase().to_string()
+                } else {
+                    c.to_lowercase().to_string()
+                };
+                result.push_str(&c);
+            }
+        }
+
+        result
+    }
+
+    fn parse(txt: &str) -> Vec<String> {
+        let mut curr = String::new();
+        let mut result = Vec::new();
+
+        for c in txt.chars() {
+            if c.is_uppercase() {
+                result.push(curr.clone());
+                curr.clear();
+            }
+
+            curr.push(c);
+        }
+
+        if !curr.is_empty() {
+            result.push(curr.clone());
+        }
+
+        result
+    }
 }
 
 #[cfg(test)]
 mod test {
-    use crate::case::{to_camel_case, to_snake_case};
+    use crate::case::{CamelCase, Case, SnakeCase};
 
     #[test]
     fn snake_case_convert_test() {
-        assert_eq!(to_snake_case("thisIsATest"), "this_is_a_test");
+        let result = SnakeCase::gen(CamelCase::parse("thisIsATest"));
+        assert_eq!(result, "this_is_a_test");
     }
 
     #[test]
     fn camel_case_convert_test() {
-        assert_eq!(to_camel_case("this_is_a_test"), "thisIsATest");
+        let result = CamelCase::gen(SnakeCase::parse("this_is_a_test"));
+        assert_eq!(result, "thisIsATest");
     }
 }
